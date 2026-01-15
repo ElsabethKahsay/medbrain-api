d59c2d1acd11ac5f550d5e60ed294922ee0eafea
ElsabethKahsay <Elsabethtesfay9@gmail.com>
2026-01-15 15:27:12 +0300

    feat: fix Prisma ORM, added  basic auth and entries modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntriesModule } from './entries/entries.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
+    EntriesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
